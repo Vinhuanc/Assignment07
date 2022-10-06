9928251907e2c4b2a4f6373554163cce427c4d3c
Vinhuanc <huann.chenn@gmail.com>
2022-10-05 21:40:44 -0500

    finished program3

---
+++ Assig07-Prog03/Assig07-Prog03.cpp
@@ -0,0 +1,41 @@
+//Huan Chen Assignment07-Program03
+#include<iostream>
+
+using namespace std;
+int num=45;
+void repchar(){
+    char a='*';
+    int num;
+    for(int i=0; i<=45; i++){
+        cout<<a;
+        i++;
+       
+    }
+}
+
+
+
+void repchar(char a){
+    for(int i=0; i<=num; i++){
+        cout<<a;
+        i++;
+    }
+}
+
+void repchar(char a, int num){
+    for(int i=0; i<=num; i++){
+        cout<<a;
+        i++;
+    }
+}
+
+int main(){
+
+    repchar();
+    cout<<"          "<<endl;
+    repchar('=');
+    cout<<"          "<<endl;
+    repchar('+',30);
+    cout<<"          "<<endl;
+}
+
